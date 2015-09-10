4e3b39f78fe1b2417380855ec82316e44906211d
tleonard2 <theresa@fiddleface.com>
2015-09-09 18:09:28 -0700

    moved to GS
    Signed-off-by: tleonard2 <theresa@fiddleface.com>

---
+++ themes/terriGS/front-page.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file adds the Home Page to the gensample Pro Theme.
+ *
+ * @author StudioPress
+ * @package gensample Pro
+ * @subpackage Customizations
+ */
+
+add_action( 'genesis_meta', 'gensample_home_genesis_meta' );
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ */
+function gensample_home_genesis_meta() {
+
+	if ( is_active_sidebar( 'home-slider' ) || is_active_sidebar( 'home-top' ) || is_active_sidebar( 'home-cta' ) || is_active_sidebar( 'home-middle' ) ) {
+
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+		add_action( 'genesis_loop', 'gensample_home_sections' );
+		add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+		add_filter( 'body_class', 'gensample_add_home_body_class' );
+
+	}
+
+}
+
+function gensample_home_sections() {
+
+	genesis_widget_area( 'home-slider', array(
+		'before' => '<div class="home-slider widget-area">',
+		'after'  => '</div>',
+	) );
+
+	genesis_widget_area( 'home-top', array(
+		'before' => '<div class="home-top widget-area">',
+		'after'  => '</div>',
+	) );
+
+	genesis_widget_area( 'mktgbuttons', array(
+		'before' => '<div class="mktgbuttons widget-area"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+	genesis_widget_area( 'home-middle', array(
+		'before' => '<div class="home-middle widget-area">',
+		'after'  => '</div>',
+	) );
+
+	genesis_widget_area( 'infobuttons', array(
+		'before' => '<div class="infobuttons widget-area"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+	genesis_widget_area( 'home-testimonial', array(
+		'before' => '<div class="home-testimonial widget-area">',
+		'after'  => '</div>',
+	) );
+
+}
+
+//* Add body class to home page
+function gensample_add_home_body_class( $classes ) {
+
+	$classes[] = 'gensample-pro-home';
+	return $classes;
+
+}
+
+genesis();
--- themes/terriGS/functions.php
+++ themes/terriGS/functions.php
@@ -1,23 +1,22 @@
 <?php
 //* Start the engine
-require_once( get_template_directory() . '/lib/init.php' );
-
-//* Setup Theme
-include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
-
-//* Set Localization (do not remove)
-load_child_theme_textdomain( 'executive', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'executive' ) );
+include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', __( 'Executive Pro Theme', 'executive' ) );
-define( 'CHILD_THEME_URL', 'http://my.studiopress.com/themes/executive/' );
-define( 'CHILD_THEME_VERSION', '3.1.1' );
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.2.0' );
+
 
 //* Add HTML5 markup structure
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
 
 //* Enqueue Scripts
 add_action( 'wp_enqueue_scripts', 'executive_load_scripts' );
@@ -30,6 +29,9 @@ function executive_load_scripts() {
 	wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic|Lato:300,700,300italic', array(), CHILD_THEME_VERSION );
 
 }
+
+
+
 
 //* ----------------- Move Entry Header -------------------------
 
@@ -53,50 +55,6 @@ function jw_add_entry_header()
 }
 
 
-
-//* ----------------- Unregister or Remove -------------------------
-
-//* Unregister layout settings
-genesis_unregister_layout( 'content-sidebar-sidebar' );
-genesis_unregister_layout( 'sidebar-content-sidebar' );
-genesis_unregister_layout( 'sidebar-sidebar-content' );
-
-//* Unregister secondary sidebar
-unregister_sidebar( 'sidebar-alt' );
-
-//* Register secondary sidebar
-add_action( 'genesis_sidebar_alt', 'genesis_do_sidebar_alt' );
-
-/** Remove Title & Description */
-remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
-remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
-
-//* Remove Genesis in-post SEO Settings
-remove_action( 'admin_menu', 'genesis_add_inpost_seo_box' );
-
-//* Remove comment form allowed tags
-add_filter( 'comment_form_defaults', 'executive_remove_comment_form_allowed_tags' );
-function executive_remove_comment_form_allowed_tags( $defaults ) {
-
-	$defaults['comment_notes_after'] = '';
-	return $defaults;
-
-}
-
-//* Load Admin Stylesheet
-add_action( 'admin_enqueue_scripts', 'executive_load_admin_styles' );
-function executive_load_admin_styles() {
-
-	wp_register_style( 'custom_wp_admin_css', get_stylesheet_directory_uri() . '/lib/admin-style.css', false, '1.0.0' );
-	wp_enqueue_style( 'custom_wp_admin_css' );
-
-}
-
-
-//* Reposition the primary navigation menu
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_header', 'genesis_do_nav' );
-
 //* ----------------- Header -------------------------
 
 
@@ -118,22 +76,58 @@ next level with small business coaching" /></div>';
 }
 
 
-// Remove the header right widget area
-unregister_sidebar( 'header-right' );
+
+
+//* ----------------- Unregister or Remove -------------------------
+
+//* Unregister layout settings
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+//* Unregister secondary sidebar
+unregister_sidebar( 'sidebar-alt' );
+
+//* Register secondary sidebar
+add_action( 'genesis_sidebar_alt', 'genesis_do_sidebar_alt' );
+
+/** Remove Title & Description */
+remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+//* Remove Genesis in-post SEO Settings
+remove_action( 'admin_menu', 'genesis_add_inpost_seo_box' );
+
+//* Remove comment form allowed tags
+add_filter( 'comment_form_defaults', 'remove_comment_form_allowed_tags' );
+function remove_comment_form_allowed_tags( $defaults ) {
+
+	$defaults['comment_notes_after'] = '';
+	return $defaults;
+
+}
+
+//* Load Admin Stylesheet
+add_action( 'admin_enqueue_scripts', 'load_admin_styles' );
+function load_admin_styles() {
+
+	wp_register_style( 'custom_wp_admin_css', get_stylesheet_directory_uri() . '/lib/admin-style.css', false, '1.0.0' );
+	wp_enqueue_style( 'custom_wp_admin_css' );
+
+}
+
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav' );
+
+
 
 
 
 //* ----------------- Widgets -------------------------
 
-//* Add support for 4-column footer widgets
+//* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
-
-//* Add support for after entry widget
-add_theme_support( 'genesis-after-entry-widget-area' );
-
-//* Relocate after entry widget
-remove_action( 'genesis_after_entry', 'genesis_after_entry_widget_area' );
-add_action( 'genesis_after_entry', 'genesis_after_entry_widget_area', 5 );
 
 //* Register widget areas
 genesis_register_sidebar( array(
